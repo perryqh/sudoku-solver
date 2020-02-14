7390c48f784501fb056b0b152172213b64cf6025
Perry Hertler <perry@hertler.org>
2020-02-13 20:58:28 -0700

    difficult puzzle

--- spec/sudoku_solver/game_spec.rb
+++ spec/sudoku_solver/game_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe SudokuSolver::Game do
   include_context 'board inputs'
   describe '#solve' do
     specify { expect(described_class.solve(unsolved_puzzle_3)).to eq(sovled_puzzle_3) }
+    specify { expect(described_class.solve(unsolved_puzzle_4)).to eq(solved_puzzle_4) }
   end
 end
